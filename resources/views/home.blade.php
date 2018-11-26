4e62a92126bd46de8c8582ac8cbdaadfc63a3a52
Pravee Wiangin <w.ptucky@gmail.com>
2018-11-26 09:02:44 +0700

    update tile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{!! csrf_token() !!}">
         
-        <title>Laravel</title>
+        <title>Vue-Laravel :: Image Gallery</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
